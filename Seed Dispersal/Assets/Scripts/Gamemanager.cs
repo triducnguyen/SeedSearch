b93c47d3b120e0d29dd671d63dabb3fb4ac0f5fa
Dryden001 <51544458+Dryden001@users.noreply.github.com>
2021-06-14 12:30:15 -0600

    Times
    added outlier removal
    added average calc
    added median calc
    added a sorted list

--- Seed Dispersal/Assets/Scripts/Gamemanager.cs
+++ Seed Dispersal/Assets/Scripts/Gamemanager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+ using System.Linq;
 
 public class Gamemanager : MonoBehaviour
 {
     public StudentData studentProfile;
     private List<float> times;
+    private List<float> sortedtimes;
     public float timescalar;
     private float starttime;
     private float endtime;
@@ -45,11 +47,38 @@ public class Gamemanager : MonoBehaviour
     }
     [SerializeField]
     private float timerstorecount;
+    private bool inloop = false;
     public void savetimes(){
-        while(times.Count > timerstorecount){
-            times.Remove(times[0]);
+        sortedtimes = times;
+        sortedtimes.Sort();
+        if(sortedtimes.Count % 2 == 0){
+            median = sortedtimes[sortedtimes.Count/2];
+        }else{
+            median = sortedtimes[sortedtimes.Count/2 + 1];
         }
+        
+        inloop = true;
+        while (inloop){
+            for(int i = 0; i < times.Count; i++){
+                if(i >= times.Count){ inloop = false;}
+                if(times[i] > median * 1.5 || times[i] < median * 0.5){
+                    times.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+        
+        while(times.Count > timerstorecount){
+            times.RemoveAt(0);
+        }
+
+
         //SaveManager.Instance.SaveStudentFile(newStudent);
            
+    }
+    private float median;
+    public void loadtimes(){
+        //insert load function
+        wait = times.Average();
     }
 }
