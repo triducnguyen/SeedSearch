decf27d2c028d01baf95a241641e1e8f48e7e430
Dryden001 <51544458+Dryden001@users.noreply.github.com>
2021-06-14 16:00:07 -0600

    Timer
    Added dummy buttons
    Added dummy code to test timer system

--- Seed Dispersal/Assets/Scripts/Gamemanager.cs
+++ Seed Dispersal/Assets/Scripts/Gamemanager.cs
@@ -9,20 +9,31 @@ public class Gamemanager : MonoBehaviour
     public StudentData studentProfile;
     private List<float> times;
     private List<float> sortedtimes;
+   
     public float timescalar;
     private float starttime;
     private float endtime;
     private float overalltime;
+
+    public Animator fillscreenwithcoloranimator;
+
+    [Header("TimerTesting")]
+    public List<float> DUMMY; 
+
     // Start is called before the first frame update
     void Start()
     {
         //SaveManager.Instance.LoadStudentData(newStudent);
+        List<float> times = new List<float>();
+        List<float> sortedtimes = new List<float>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(endtime - starttime > 10*60){
+            fillscreenwithcoloranimator.SetBool("Fillscreenwithcolor", true);
+        }
     }
 
     //John Add
@@ -30,14 +41,18 @@ public class Gamemanager : MonoBehaviour
         section = currentSection;
         starttime = Time.deltaTime;
         StartCoroutine(Hinttimer());
+        Debug.Log("start time: " + starttime);
     }
     public void endtimer(){
         endtime = Time.deltaTime;
         overalltime = endtime - starttime;
+        //times.Add(overalltime);
         times.Add(overalltime);
+        Debug.Log("end time: " + endtime + " And overall time: " + overalltime);
     }
     private string section;
     public float wait;
+    public float avg;
     public GameObject hintObject;
     public Text hint;
     IEnumerator Hinttimer(){
@@ -56,29 +71,52 @@ public class Gamemanager : MonoBehaviour
         }else{
             median = sortedtimes[sortedtimes.Count/2 + 1];
         }
-        
-        inloop = true;
-        while (inloop){
-            for(int i = 0; i < times.Count; i++){
-                if(i >= times.Count){ inloop = false;}
-                if(times[i] > median * 1.5 || times[i] < median * 0.5){
-                    times.RemoveAt(i);
-                    break;
+        if(times.Count > timerstorecount / 4){
+            /*inloop = true;
+            while (inloop){
+                for(int i = 0; i < times.Count; i++){
+                    if(i >= times.Count){ inloop = false;}
+                    if(times[i] > median * 1.5 || times[i] < median * 0.5){
+                        times.RemoveAt(i);
+                        break;
+                    }
                 }
+            }*/
+            
+            while(times.Count > timerstorecount){
+                times.RemoveAt(0);
             }
-        }
-        
-        while(times.Count > timerstorecount){
-            times.RemoveAt(0);
         }
 
 
         //SaveManager.Instance.SaveStudentFile(newStudent);
-           
+
+
+        //comment below
+        Debug.Log("Times" + times.ToArray());
+        foreach(float time in times)
+        {
+            Debug.Log(time);
+        }
+        Debug.Log("sorted");
+        foreach(float time in sortedtimes)
+        {
+            Debug.Log(time);
+        }
+        Debug.Log("average time" + avg);  
+        Debug.Log("Median time" + median);  
     }
     private float median;
     public void loadtimes(){
-        //insert load function
-        wait = times.Average();
+        times = DUMMY; 
+
+        //comment above
+        Debug.Log("init Times");
+        foreach(float time in times)
+        {
+            Debug.Log(time);
+        }
+        avg = times.Average();
+        wait = avg * 2;
     }
 }
