6633c8a2529dfc335be7f03defbde675f91146ab
Dryden001 <51544458+Dryden001@users.noreply.github.com>
2021-06-15 09:39:29 -0600

    List testing
    attempted splitting lists

--- Seed Dispersal/Assets/Scripts/Gamemanager.cs
+++ Seed Dispersal/Assets/Scripts/Gamemanager.cs
@@ -60,10 +60,11 @@ public class Gamemanager : MonoBehaviour
     }
     [SerializeField]
     private float timerstorecount;
-    private bool inloop = false;
+    private bool Reloop = false;
     public void savetimes(){
         //sortedtimes = times;
-        List<float> sortedtimes = times.ToList();
+        //List<float> sortedtimes = times.ToList();
+        List<float> sortedtimes = times;
         //List<float> sortedtimes = new List<float>(times);
         sortedtimes.Sort();
         if(sortedtimes.Count % 2 == 0){
@@ -72,13 +73,15 @@ public class Gamemanager : MonoBehaviour
             median = sortedtimes[sortedtimes.Count/2 + 1];
         }
         if(times.Count > timerstorecount / 4){
-            inloop = true;
-            //while (inloop){
+            Reloop = true;
+            Debug.Log("starting for loop");
+            //while (Reloop){
                 for(int i = 0; i < times.Count; i++){
                     if(times.Count > i){
                         if(times[i] > median * 1.5 || times[i] < median * 0.5){
                             times.RemoveAt(i);
                             Debug.Log("removing value " + times[i]);
+                            Reloop = true;
                             i--;
                         }
                     } else{Debug.Log("EROR");}
@@ -95,35 +98,17 @@ public class Gamemanager : MonoBehaviour
         }
 
 
-        //SaveManager.Instance.SaveStudentFile(newStudent);
-
-
-        //comment below
-        Debug.Log("Times" + times.ToArray());
-        foreach(float time in times)
-        {
-            Debug.Log(time);
-        }
-        Debug.Log("sorted");
-        foreach(float time in sortedtimes)
-        {
-            Debug.Log(time);
-        }
-        Debug.Log("average time" + avg);  
-        Debug.Log("Median time" + median);  
+        //SaveManager.Instance.SaveStudentFile(newStudent); 
     }
     [SerializeField] private float median;
     public void loadtimes(){
         //times = DUMMY;
-        List<float> times = DUMMY.ToList();
+        //List<float> times = DUMMY.ToList();
+        List<float> times = DUMMY;
         //List<float> times = new List<float>(DUMMY); 
 
         //comment above
-        Debug.Log("init Times");
-        foreach(float time in times)
-        {
-            Debug.Log(time);
-        }
+        
         avg = times.Average();
         wait = avg * 2;
     }
