10678987c01e0dc156bb7f0b6ea778ba395cfe31
Dryden001 <51544458+Dryden001@users.noreply.github.com>
2021-07-08 12:01:22 -0600

    added dandelion seed movement

--- Seed Dispersal/Assets/Scripts/john_gamemanager.cs
+++ Seed Dispersal/Assets/Scripts/john_gamemanager.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class john_gamemanager : MonoBehaviour
 {
+    [Header("Watering can")]
     public GameObject wateringcan;
     public GameObject wateringcanhome;
     public GameObject wateringcancheckpoint;
     private string canstate;
     public GameObject waterincan;
     [SerializeField] private float smooth;
+
+    [Header("Dandelion")]
+    public GameObject dandelionseed;
+    [SerializeField] private float dandelionspeed;
+    [SerializeField] private float dandelionseedflyheight;
+    private string seedstate;
+    private Vector3 target;
+
     // Start is called before the first frame update
     void Start()
     {
         wateringcanhome.transform.position = wateringcan.transform.position;
         waterincan.SetActive(false);
-        canstate = "water";
-        movewateringcan();
+        canstate = "return";
+        seedstate = " ";
+        dandelionseed.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && canstate == "water")
+        if (Input.GetMouseButtonDown(0))
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -31,7 +41,24 @@ public class john_gamemanager : MonoBehaviour
                 var selection = hit.transform;
                 if (selection.CompareTag("wateringcan"))
                 {
-                    canstate = "tipping";
+                    if(canstate == "water"){
+                        canstate = "tipping";
+                    } else if(canstate == "tipped"){
+                        canstate = "return";
+                        waterincan.SetActive(false);
+                        movewateringcan();
+                    } else if(canstate == "return"){
+                        canstate = "water";
+                        waterincan.SetActive(false);
+                        movewateringcan();
+                    }
+                }
+                else if(selection.CompareTag("dandelionflower") && seedstate == " "){
+                    dandelionseed.SetActive(true);
+                    seedstate = "idle";
+                }else if(selection.CompareTag("island") && dandelionseed.activeInHierarchy){
+                    seedstate = "fly";
+                    target = hit.point;
                 }
             }
         }
@@ -43,6 +70,18 @@ public class john_gamemanager : MonoBehaviour
                 waterincan.SetActive(true);
             }
         }
+        if(seedstate == "fly"){
+            dandelionseed.transform.position = Vector3.MoveTowards(dandelionseed.transform.position, target + new Vector3(0, dandelionseedflyheight, 0), dandelionspeed * Time.deltaTime);
+            if(dandelionseed.transform.position == target + new Vector3(0, dandelionseedflyheight, 0)){
+                seedstate = "drop";
+            }
+        }else if(seedstate == "drop"){
+            dandelionseed.transform.position = Vector3.MoveTowards(dandelionseed.transform.position, target, 2* dandelionspeed * Time.deltaTime);
+            if(dandelionseed.transform.position == target){
+                seedstate = "plant";
+            }
+        }
+        
         
     }
 
